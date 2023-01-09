d6b00428a6d14add1ac890cf9c04708a2b4ef0ae
JustinAronson <justinaronson@gmail.com>
2023-01-09 16:52:14 -0600

    Grid of 9 towers

--- generate.py
+++ generate.py
@@ -8,14 +8,29 @@ height = 1
 
 x = 0
 y = 0
-z = height/2
 
-pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+for row in range(0, 3):
+    y = 0
 
-x += 1
-z += 1
+    for column in range(0, 3):
+        length = 1
+        width = 1
+        height = 1
+        z = height/2
+        
+        for i in range (0, 10):
+            pyrosim.Send_Cube(name="Box" + str(i) + str(row) + str(column), pos=[x, y, z], size=[length, width, height])
 
-pyrosim.Send_Cube(name="Box2", pos=[x, y, z], size=[length, width, height])
+            z += height
+
+            length *= 0.9
+            width *= 0.9
+            height *= 0.9
+
+        y += 1
+    x += 1
+
+#pyrosim.Send_Cube(name="Box2", pos=[x, y, z], size=[length, width, height])
 
 
 pyrosim.End()
