f2742910df11064a4ed9e01195c08e4875b864cd
JustinAronson <justinaronson@gmail.com>
2023-01-09 16:37:12 -0600

    Created second box

--- generate.py
+++ generate.py
@@ -1,15 +1,21 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
 x = 0
 y = 0
 z = height/2
 
 pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+
+x += 1
+z += 1
+
+pyrosim.Send_Cube(name="Box2", pos=[x, y, z], size=[length, width, height])
+
 
 pyrosim.End()
