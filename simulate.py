d6ad57241857ddc722496bc0f7b96a78bb6344bf
JustinAronson <justinaronson@gmail.com>
2023-01-09 16:21:31 -0600

    Finished one link

--- simulate.py
+++ simulate.py
@@ -3,8 +3,10 @@ import time
 
 physicsClient = p.connect(p.GUI)
 
+p.loadSDF("box.sdf")
+
 for i in range(0, 1000):
-    time.sleep(1/2000)
+    time.sleep(1/60)
     p.stepSimulation()
 
     print(i)
