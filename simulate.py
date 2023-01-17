3216bc8d8b6c8d53b889558faee25d76bd209753
JustinAronson <justinaronson@gmail.com>
2023-01-16 20:36:56 -0600

    Added motors

--- simulate.py
+++ simulate.py
@@ -4,11 +4,12 @@ import pybullet_data
 import generate
 import pyrosim.pyrosim as pyrosim
 import numpy
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.setGravity(0, 0, -9.8)
+p.setGravity(0, 0, -18.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
@@ -17,7 +18,7 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-loopIterations = 200
+loopIterations = 800
 backLegSensorValues = numpy.zeros(loopIterations)
 frontLegSensorValues = numpy.zeros(loopIterations)
 
@@ -27,6 +28,14 @@ for i in range(0, loopIterations):
 
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+
+
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b'Torso_BackLeg',
+        controlMode = p.POSITION_CONTROL, targetPosition = -3.14/4, maxForce = 200)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b'Torso_FrontLeg',
+        controlMode = p.POSITION_CONTROL, targetPosition = 3.14/4, maxForce = 200)
+
 
 p.disconnect()
 
