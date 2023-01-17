87ac155794a2daa3f442d7617d5b420c88fe3906
JustinAronson <justinaronson@gmail.com>
2023-01-16 21:20:25 -0600

    Moved away from origin

--- simulate.py
+++ simulate.py
@@ -5,11 +5,13 @@ import generate
 import pyrosim.pyrosim as pyrosim
 import numpy
 import random
+import math
+import matplotlib.pyplot as matplotlib
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.setGravity(0, 0, -18.8)
+p.setGravity(0, 0, -29.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
@@ -18,23 +20,38 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-loopIterations = 800
+amplitudeBack = numpy.pi/4
+frequencyBack = 3
+phaseOffsetBack = 0
+amplitudeFront = numpy.pi/4
+frequencyFront = 6
+phaseOffsetFront = numpy.pi/4
+
+loopIterations = 1000
 backLegSensorValues = numpy.zeros(loopIterations)
 frontLegSensorValues = numpy.zeros(loopIterations)
 
+targetAngles = numpy.linspace(0, 2 * numpy.pi, loopIterations)
+targetAnglesBack = amplitudeBack * numpy.sin(targetAngles * frequencyBack + phaseOffsetBack)
+targetAnglesFront = amplitudeFront * numpy.sin(targetAngles * frequencyFront + phaseOffsetFront)
+
+# matplotlib.plot(targetAngles)
+# matplotlib.show()
+# exit()
+
 for i in range(0, loopIterations):
-    time.sleep(1/20)
+    time.sleep(1/240)
     p.stepSimulation()
 
-    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    # backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    # frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
 
 
     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b'Torso_BackLeg',
-        controlMode = p.POSITION_CONTROL, targetPosition = -3.14/4, maxForce = 200)
+        controlMode = p.POSITION_CONTROL, targetPosition = targetAnglesBack[i], maxForce = 200)
     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b'Torso_FrontLeg',
-        controlMode = p.POSITION_CONTROL, targetPosition = 3.14/4, maxForce = 200)
+        controlMode = p.POSITION_CONTROL, targetPosition = targetAnglesFront[i], maxForce = 200)
 
 
 p.disconnect()
