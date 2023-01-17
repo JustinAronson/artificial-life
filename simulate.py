e7c0b12f117e2b00e87b9e4cd48ab43ba8ff909b
JustinAronson <justinaronson@gmail.com>
2023-01-16 20:19:57 -0600

    Added sensors and their plots

--- simulate.py
+++ simulate.py
@@ -2,6 +2,8 @@ import pybullet as p
 import time
 import pybullet_data
 import generate
+import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -13,10 +15,20 @@ robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf")
 
-for i in range(0, 1000):
+pyrosim.Prepare_To_Simulate(robotId)
+
+loopIterations = 200
+backLegSensorValues = numpy.zeros(loopIterations)
+frontLegSensorValues = numpy.zeros(loopIterations)
+
+for i in range(0, loopIterations):
     time.sleep(1/20)
     p.stepSimulation()
 
-    print(i)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
 p.disconnect()
+
+numpy.save('data//backLegVals', backLegSensorValues)
+numpy.save('data//frontLegVals', frontLegSensorValues)
