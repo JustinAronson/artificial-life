fce6b3b137783f74446cfc50e4fde49e03af633a
JustinAronson <justinaronson@gmail.com>
2023-02-27 23:44:18 -0600

    Adding fitness and robot plans

--- constants.py
+++ constants.py
@@ -7,17 +7,17 @@ amplitudeFront = numpy.pi/4
 frequencyFront = 6
 phaseOffsetFront = numpy.pi/4
 
-loopIterations = 10000
+loopIterations = 800
 
-numberOfGenerations = 100
-populationSize = 100
+numberOfGenerations = 50
+populationSize = 50
 numTrials = 5
 
 numSensorNeurons = 4
 numMotorNeurons = 8
 numHiddenNeurons = 4
 
-motorJointRange = 0.2
+motorJointRange = 0.4
 
 minLinks = 4
 maxDepth = 4
